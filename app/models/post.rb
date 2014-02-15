b8277b95aa70ed4ec598a231d2eafe0bda4f661f
Jeremy Shouse <jtshouse@gmail.com>
2014-02-14 20:49:38 -0700

    Add permalink style url slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :text, presence: true
+  validates_uniqueness_of :title
+  
+  def to_param
+    title
+  end
+  
+  def self.find(input)
+    if input.to_i != 0
+      super
+    else
+      find_by_title(input)
+    end
+  end
 end
